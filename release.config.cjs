72921b745b03e3274fc27fc24678da0f9e151ee6
paalamugan <paalamugan@gmail.com>
2023-08-28 11:00:18 +0530

    🩹 chore(release): update release.config.js

---
+++ release.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+};
